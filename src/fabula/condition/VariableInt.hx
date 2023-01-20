6aafe2a87d2c21b68dbb792f6ef891083e7920f3
Ludovic Bas <loudoweb@gmail.com>
2023-01-20 16:43:52 +0100

    allow ++, +2, etc. on variable int

--- src/fabula/condition/VariableInt.hx
+++ src/fabula/condition/VariableInt.hx
@@ -12,10 +12,41 @@ class VariableInt extends Variable<Int>
 	override public function set(value:String):Bool
 	{
 		var temp:Int = this.value;
-		this.value = Std.parseInt(value);
+
+		switch (value)
+		{
+			case "+" | "++":
+				this.value++;
+			case "-" | "--":
+				this.value--;
+			case extract(_) => result:
+				switch (result.operation)
+				{
+					case "+":
+						this.value += result.subValue;
+					case "-":
+						this.value -= result.subValue;
+					default:
+						this.value = result.subValue;
+				}
+		}
 		if (temp != this.value)
 			return true;
 		return false;
+	}
+
+	function extract(str:String):{operation:String, subValue:Int}
+	{
+		var operation = str.charAt(0);
+		var subValue = 0;
+		if (operation == "+" || operation == "-")
+		{
+			subValue = Std.parseInt(str.substring(1));
+		} else
+		{
+			subValue = Std.parseInt(str);
+		}
+		return {operation: operation, subValue: subValue};
 	}
 
 	override function convert(value:String):Int
