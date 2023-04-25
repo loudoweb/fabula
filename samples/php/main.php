e581638d6e5ffa3383ee571c0d9976042a776630
loudoweb <loudoweb@gmail.com>
2023-04-25 11:34:43 +0200

    add reset method to php sample and fix bugs

--- samples/php/main.php
+++ samples/php/main.php
@@ -1,6 +1,5 @@
 <?php
-
-set_include_path(get_include_path().PATH_SEPARATOR.__DIR__.'/php/lib');
+set_include_path('../../dist/php/lib');
 spl_autoload_register(
 	function($class){
 		$file = stream_resolve_include_path(str_replace('\\', '/', $class) .'.php');
@@ -22,33 +21,45 @@ session_start(); // start php session
 $xml = '<?xml version="1.0" encoding="utf-8" ?><data><sequence id="Talk_to_kid"><event speaker="kid_sad" if="kid_angry" text="I won`\'t talk to you anymore..." exit="true"/><event speaker="kid" if="!kid_happy" text="Hi, have you seen my new toy ? I love it!"/><event speaker="kid" text="Do you want to play with me?"><choice id="kid_angry" text="Your toy looks like crap!"/><choice id="kid_happy" text="Yes, I love it!" target="kid_happy"/><choice id="goto_elsewhere" text="Leave him alone" exit="true"/></event><event if="kid_angry" speaker="kid_angry" text="You\'re so mean!" exit="true"/><event id="kid_happy" speaker="kid_happy" ><text><![CDATA[Thanks! <font color="#2281AB">Let\'s play!</font>]]></text></event></sequence></data>';
 $story = new Fabula(\Array_hx::wrap([$xml]));
 $story->selectSequence("Talk_to_kid");
-$event = $story->getNextEvent();
-$choice = $event->getChoices();
+
+if (isset($_POST["reset"])) {
+	//clear session (= clear list of completed event and choices + clear current event id)
+	session_destroy();
+	echo "reset";
+	return;
+}else if(isset($_SESSION['ArrayCompleteId'])){
+	//get the completed list only if != null
+	$story->completedID = $_SESSION['ArrayCompleteId'];
+}
 
 if (isset($_POST["id"])) {
-	$story->completedID = $_SESSION['ArrayCompleteId'];
+	//handle selected choice
+
 	$story->currentSequence->currentId = $_SESSION['EventId'];
+	$event = $story->getCurrentEvent();
 
 	//Select the choice with choice id
     $story->selectChoice($id = $_POST["id"], $index = null);
 	$event = $story->getNextEvent();
 
+	
+
 	if($event != null){
+		$choice = $event->getChoices();
+		//stock current event id in session
+		$_SESSION['EventId'] = $story->currentSequence->currentId;
+		//stock the complete id array
+		$_SESSION['ArrayCompleteId'] = $story->completedID;
+
 
 		//send speaker, label and choice to HTML
 		$response = [
 			'speaker' => json_encode($event->speaker),
 			'label'  => json_encode($event->text),
 			'choice' => json_encode($choice),
-			'logs' => ob_get_contents()
+			'fabula' => explode("\r\n", ob_get_contents()),
+			'logs' => $story->completedID
 		];
-
-		//stock event id in complete id array
-		$_SESSION['EventId'] = $story->currentSequence->currentId;
-		$story->completedID[] = $_SESSION['EventId'];
-
-		//stock the complete id array
-		$_SESSION['ArrayCompleteId'] = $story->completedID;
 	} else {
 		$response = null;
 	}
@@ -57,23 +68,22 @@ if (isset($_POST["id"])) {
 	header('Content-Type: application/json');
 	echo json_encode($response);
 } else {
+	//get the first event
 	$event = $story->getNextEvent();
 	$choice = $event->getChoices();
 
-	$story->completedID = $_SESSION['ArrayCompleteId'];
+	//stock current event id in session
+	$_SESSION['EventId'] = $story->currentSequence->currentId;	
+	//stock the complete id array
+	$_SESSION['ArrayCompleteId'] = $story->completedID;
+
 	$response = [
 		'speaker' => json_encode($event->speaker),
 		'label'  => json_encode($event->text),
 		'choice' => json_encode($choice),
-        'logs' => ob_get_contents()
+		'fabula' => explode("\r\n", ob_get_contents()),
+		'logs' => $story->completedID
 	];
-
-	//stock event id in complete id array
-	$_SESSION['EventId'] = $story->currentSequence->currentId;
-	$story->completedID[] = $_SESSION['EventId'];
-	
-	//stock the complete id array
-	$_SESSION['ArrayCompleteId'] = $story->completedID;
 
 	ob_clean();
 	header('Content-Type: application/json');
